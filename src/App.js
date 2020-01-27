fd35e2bc4c9d6406e88acf1aeb49a99bc27f6547
Logan <logan.christopher@comcast.net>
2020-01-27 03:22:57 -0600

    scrub

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Unit1: "nautical",
+      Unit2: "nautical",
+      defVal1: null,
+      defVal2: null
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    let num = e.target.value;
+    let unit1 = this.state.Unit1;
+    let unit2 = this.state.Unit2;
+
+    if (unit1 == "nautical" && unit2 == "miles") {
+      num = num * 1.15078;
+      this.setState({ defVal2: num });
+    } else if (unit1 == "nautical" && unit2 == "kilometers") {
+      num = num * 1.852;
+      this.setState({ defVal2: num });
+    } else if (unit1 == "nautical" && unit2 == "feet") {
+      num = num * 6076.11548556;
+      this.setState({ defVal2: num });
+    } else if (unit1 == "miles" && unit2 == "nautical") {
+      num = num / 1.15078;
+      this.setState({ defVal2: num });
+    } else if (unit1 == "miles" && unit2 == "kilometers") {
+      num = num * 1.609344;
+      this.setState({ defVal2: num });
+    } else if (unit1 == "miles" && unit2 == "feet") {
+      num = num * 5280;
+      this.setState({ defVal2: num });
+    } else if (unit1 == "kilometers" && unit2 == "nautical") {
+      num = num / 1.852;
+      this.setState({ defVal2: num });
+    } else if (unit1 == "kilometers" && unit2 == "miles") {
+      num = num / 1.609344;
+      this.setState({ defVal2: num });
+    } else if (unit1 == "kilometers" && unit2 == "feet") {
+      num = num / 0.0003048;
+      this.setState({ defVal2: num });
+    } else if (unit1 == "feet" && unit2 == "nautical") {
+      num = num / 6076.11549;
+      this.setState({ defVal2: num });
+    } else if (unit1 == "feet" && unit2 == "miles") {
+      num = num / 5280;
+      this.setState({ defVal2: num });
+    } else if (unit1 == "feet" && unit2 == "kilometers") {
+      num = num * 0.0003048;
+      this.setState({ defVal2: num });
+    } else {
+      this.setState({ defVal2: num });
+    }
+  }
+
+  handleDrop = (e, unitNum) => {
+    this.setState({[unitNum]: e.target.value});
+  }
+
+  UnitBox = (props) => {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+        <h1>{props.unitNum}</h1>
+          <select onChange={(e) => this.handleDrop(e, props.unitNum)}>
+            <option value="nautical">Nautical</option>
+            <option value="miles">Miles</option>
+            <option value="kilometers">Kilometers</option>
+            <option value="feet">Feet</option>
+          </select>
+          <label>
+            <input readonly={props.readonly} type="text" defaultValue={props.value} onChange={(e) => this.handleChange(e, props.unitNum)} />
+          </label>
+        </form>
+      </div>
+    )
+  }
+
+  render() {
+    let val1 = this.state.defVal1;
+    let val2 = this.state.defVal2;
+
+    return (
+      <div>
+        <h3>Welcome to the "Nos is a scrub" unit convertor.</h3>
+        <this.UnitBox unitNum={"Unit1"} value={val1}/>
+        <this.UnitBox readonly="readonly" unitNum={"Unit2"} value={val2}/>
+      </div>
+    );
+  }
+}
+
+export default App;
